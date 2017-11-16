c0ebfa76e0adcd441e54c290a4564acb8449cfbb
Martijn Rietveld <>
2017-11-16 16:17:34 +0100

    Social media links

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,13 @@
 
 
     <!-- <div class="container"> -->
-        <!-- <a href="http://twitter.nl">Twitter link</a> -->
+    <div class="">
+        <p>Follow us on:</p>
+        <a href="https://www.twitter.com/ScreenhitStudio">Twitter</a>
+        <a href="https://www.facebook.com/Screenhit-Studios-192750167951140/">Facebook</a>
+        <a href="https://www.instagram.com/screenhit_studios/?hl=nl">Instagram</a>
+    </div>
+
 
     <!-- </div> -->
 </div>
