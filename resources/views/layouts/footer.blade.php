7c27fea81b07711317b11456dc88986d8b828a96
Martijn Rietveld <>
2017-11-16 14:18:31 +0100

    Form style

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,8 @@
-<div class="footer">
+<div class="footer contain-width">
+
+
     <!-- <div class="container"> -->
-        <a href="http://twitter.nl">Twitter link</a>
+        <!-- <a href="http://twitter.nl">Twitter link</a> -->
 
     <!-- </div> -->
 </div>
