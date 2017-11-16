5b213d38ff91bde99e21eccd517859b7c689216c
Snira <gbhavelaar@gmail.com>
2017-11-16 14:50:36 +0100

    secure

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,19 @@
 
 Auth::routes();
 
+
 Route::get('/', 'HomeController@index')->name('home');
 Route::post('/signup', 'HomeController@letterSignUp')->name('home.signup');
 
 Route::resource('/post','PostController');
 
-Route::get('/admin', 'DashboardController@index')->name('admin');
-Route::get('/admin/posts', 'PostController@index')->name('admin.posts');
-Route::get('/admin/posts/create','PostController@create')->name('admin.posts.create');
-Route::post('/admin/posts/store','PostController@store')->name('admin.posts.store');
-Route::get('/admin/posts/{id}','PostController@edit')->name('admin.posts.edit');
-Route::post('/admin/posts/update/{id}', 'PostController@update')->name('admin.posts.update');
-Route::get('/admin/posts/delete/{id}','PostController@destroy')->name('admin.posts.delete');
+
+Route::get('/admin', 'DashboardController@index')->name('admin')->middleware('admin');
+Route::get('/admin/posts', 'PostController@index')->name('admin.posts')->middleware('admin');
+Route::get('/admin/posts/create','PostController@create')->name('admin.posts.create')->middleware('admin');
+Route::post('/admin/posts/store','PostController@store')->name('admin.posts.store')->middleware('admin');
+Route::get('/admin/posts/{id}','PostController@edit')->name('admin.posts.edit')->middleware('admin');
+Route::post('/admin/posts/update/{id}', 'PostController@update')->name('admin.posts.update')->middleware('admin');
+Route::get('/admin/posts/delete/{id}','PostController@destroy')->name('admin.posts.delete')->middleware('admin');
 
 
